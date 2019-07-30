2fe0ba330bf4ccbc1c7f29611792e4d0c2a45acb
vanman9599 <vanjordan46@hotmail.com>
2019-07-30 13:38:31 -0700

    added restricted middleware

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const providerRouter = require('../providers/provider-router.js');
 const staffRouter = require('../providers/staff-router.js');
 const childRouter = require('../child/child-router.js');
 const immunizationRouter = require('../immunizations/immunization-router.js');
+const restricted = require('../auth/restricted-middleware.js');
 const express = require('express')
 const helmet = require('helmet');
 const cors = require('cors')
@@ -16,12 +17,12 @@ server.use(express.json());
 server.use(cors());
 
 server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
-server.use('/api/parents', parentsRouter);
-server.use('/api/providers', providerRouter);
-server.use('/api/children', childRouter);
-server.use('/api/staff', staffRouter);
-server.use('/api/immunizations', immunizationRouter)
+server.use('/api/users', restricted, usersRouter);
+server.use('/api/parents', restricted, parentsRouter);
+server.use('/api/providers', restricted, providerRouter);
+server.use('/api/children', restricted, childRouter);
+server.use('/api/staff', restricted, staffRouter);
+server.use('/api/immunizations', restricted, immunizationRouter)
 server.get('/', (req, res) => {
   res.send("Immunization Tracker API");
 });
