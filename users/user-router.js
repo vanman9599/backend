19ec2fee91be545cfefc5c267dbd498acc1a300c
vanman9599 <vanjordan46@hotmail.com>
2019-07-30 19:11:29 -0700

    fixed put operation for users

--- users/user-router.js
+++ users/user-router.js
@@ -8,12 +8,13 @@ router.put('/:id', async (req, res) => {
     try{
         const user = await Users.findById(id); //TODO Implment this function
         if(user){
-            const updatedUser = await User.update(changes, id)
+            const updatedUser = await Users.update(changes, id)
             res.json(updatedUser);
         }else{
             res.status(404).json({ message: 'Could not find user with given Id'})
         }
     }catch(err){
+        console.log("error", err);
         res.status(500).json({ message: 'failed to update user' })
     }
 })
